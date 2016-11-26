c3fc7dc41928e5f258736cd3d5147a5e00ec644b
Karolina <karolajnaa@gmail.com>
2016-11-26 20:35:05 +0100

    Added versions of program with buttons using inner classes

--- ButtonsAndFrames/src/com/Layouts/BorderLayoutFrameInner.java
+++ ButtonsAndFrames/src/com/Layouts/BorderLayoutFrameInner.java
@@ -1,35 +1,46 @@
 package com.Layouts;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
-public class MyFrame extends JFrame {
-	
-	public MyFrame() {
-		super("Border layout, check it out!");
-		setSize(300, 250);
-		setLocation(200, 150);
+public class BorderLayoutFrameInner extends JFrame{
+
+	public static void main (String[] args) {
+		new BorderLayoutFrameInner();
+	}
+
+	public BorderLayoutFrameInner() {
+		super("Border layout with an inner class!");
+		setSize(500, 500);
+		setLocation(400, 400);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		JButton north = new JButton ("North");
 		JButton east = new JButton ("East");
 		JButton west = new JButton ("West");
 		JButton south = new JButton ("South");
 		JButton center = new JButton ("Center");		
-	
+
 		BorderLayout border = new BorderLayout();
-		
-		
+
 		add(north, border.NORTH);
 		add(east, border.EAST);
 		add(west, border.WEST);
 		add(south, border.SOUTH);
 		add(center, border.CENTER);
-		
+
 		setVisible(true);
 	}
-}
 
+	void run() {
+		EventQueue.invokeLater(new Runnable (){
+			@Override
+			public void run(){
+				new BorderLayoutFrameInner();
+			}
+		});
+	}
+}
---
+++ ButtonsAndFrames/src/com/Layouts/GridFrameInnerClass.java
@@ -0,0 +1,36 @@
+package com.Layouts;
+
+import java.awt.EventQueue;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class GridFrameInnerClass extends JFrame {
+
+	public GridFrameInnerClass() {
+		super("Check out this awesome grid frame using an inner class!");
+		setBounds(200, 200, 700, 400);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(new GridLayout(5, 0));
+
+		for (int i=0; i < 15; i++)
+			add(new JButton(""+ (i+1)));
+
+		setVisible(true);
+	}
+
+	void run() { 
+		EventQueue.invokeLater(new Runnable(){
+
+			@Override
+			public void run() {
+				new GridFrameInnerClass();
+			}
+		});		
+	}
+	public static void main (String[] args){
+		new GridFrameInnerClass().run();
+	}
+}
+
+
