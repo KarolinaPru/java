34beaa566bd6474893c722bce3aad493d49d54a9
KarolinaPru <karolajnaa@gmail.com>
2016-12-04 10:32:11 +0100

    Deleted loose projects and fixed MyFrame from Swing class

--- EventHandling/com/karolina2/MyFrame.java
+++ EventHandling/com/karolina2/MyFrame.java
@@ -1,7 +1,10 @@
 package com.karolina2;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JComponent;
@@ -11,51 +14,70 @@ import javax.swing.SwingUtilities;
 
 import com.karolina2.SwingSampleWithLambdaAndMyComponent.MyComponent;
 
-public class MyFrame extends JFrame{
-	
+public class MyFrame extends JFrame
+{
+
 	private static final int WIDTH = 300;
 	private static final int HEIGHT = 200;
-	
-	public static void main(String[] args){
-			new MyFrame.run() {
-			}
-			
-	public MyFrame() {
-		JButton buttonOne = new JButton("North");
-		JButton buttonTwo = new JButton("East");
-		JButton buttonThree = new JButton("South");
-		JButton buttonFour = new JButton("West");
-		JButton buttonFive = new JButton("Center");		
-		
-		MyFrame container = new MyFrame();
-		setSize(WIDTH, HEIGHT);
-		
-		
-		void run() {
-			
-				JFrame frame = new JFrame("Frame with buttons");
-				frame.setSize(WIDTH, HEIGHT);    
-				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				
-				frame.add(buttonOne, BorderLayout.NORTH);
-				frame.add(buttonTwo, BorderLayout.EAST);
-				frame.add(buttonThree, BorderLayout.SOUTH);
-				frame.add(buttonFour, BorderLayout.WEST);
-				frame.add(buttonFive, BorderLayout.CENTER);
-		
-				frame.getContentPane().add(closeButton);
-				frame.pack();
-				frame.setVisible(true);                  
-			}
-		} 
-		
-		
+	private JButton buttonOne;
+	private JButton buttonTwo;
+	private JButton buttonThree;
+	private JButton buttonFour;
+	private JButton buttonFive;
+	private JFrame frame;
+	private JButton closeButton;
+
+	public static void main(String[] args)
+	{
+		new MyFrame().run();
 	}
-		
-		
-			EventQueue.invokeLater(new Runnable(){
-				
-				}
-			});
+
+	public MyFrame()
+	{
+		setSize(WIDTH, HEIGHT);
+		buttonOne = new JButton("North");
+		buttonTwo = new JButton("East");
+		buttonThree = new JButton("South");
+		buttonFour = new JButton("West");
+		buttonFive = new JButton("Center");
+		closeButton = new JButton("Close");
+		closeButton.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				frame.setVisible(false);
+			}
+		});
+	}
+
+	void run()
+	{
+
+		frame = new JFrame("Frame with buttons");
+		frame.setSize(WIDTH, HEIGHT);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		frame.add(buttonOne, BorderLayout.NORTH);
+		frame.add(buttonTwo, BorderLayout.EAST);
+		frame.add(buttonThree, BorderLayout.SOUTH);
+		frame.add(buttonFour, BorderLayout.WEST);
+		frame.add(buttonFive, BorderLayout.CENTER);
+
+		frame.getContentPane().add(closeButton);
+		frame.pack();
+		frame.setVisible(true);
+
+		EventQueue.invokeLater(new Runnable()
+		{
+
+			@Override
+			public void run()
+			{
+				// TODO Auto-generated method stub
+
+			}
+
+		});
 	}
 }
