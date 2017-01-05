9f3eb39039492782139d262b4e82d0eee0bbac83
Karolina <karolajnaa@gmail.com>
2017-01-05 20:46:43 +0100

    Refactoring

--- PropertyDemo2/application/Main.java
+++ PropertyDemo2/application/Main.java
@@ -6,7 +6,6 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
