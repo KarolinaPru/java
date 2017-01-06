e7c5d246f5fabad5bd19782096ef0d2422e45fb5
Karolina <karolajnaa@gmail.com>
2017-01-06 20:40:48 +0100

    Styling (css not working)

--- QuadraticFunctionDrawingTool/src/application/Coordinate.java
+++ QuadraticFunctionDrawingTool/src/application/Coordinate.java
@@ -17,20 +17,4 @@ public class Coordinate
 	public double getY(){
 		return y;
 	}
-	
-    @Override
-    public boolean equals(Object other) {
-        boolean result = false;
-        if (other instanceof Coordinate) {
-        	Coordinate that = (Coordinate) other;
-            result = (this.getX() == that.getX() && this.getY() == that.getY());
-        }
-        return result;
-    }
-
-    @Override
-    public int hashCode() {
-    	Double hashCode = new Double((41 * (41 + getX()) + (getY() * 3)));
-        return  hashCode.intValue();
-    }
 }
