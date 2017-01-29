efb9ac26efefbd3936c30e4231337c0d610a7d4e
KarolinaPru <karolajnaa@gmail.com>
2017-01-29 17:03:51 +0100

    Set main in Clock

--- Clock/src/controller/MainWindowController.java
+++ Clock/src/controller/MainWindowController.java
@@ -1,6 +1,15 @@
 package controller;
 
+import javafx.stage.Stage;
+
 public class MainWindowController
 {
+	private Stage primaryStage;
+	private Main main;
 
+	public void setMain(Main main, Stage primaryStage)
+	{
+		this.main = main;
+		this.primaryStage = primaryStage;
+	}
 }
