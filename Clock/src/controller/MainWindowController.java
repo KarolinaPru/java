4afd8cf8d8e0dd6d6b9a194fdb155517ced02a02
Karolina <karolajnaa@gmail.com>
2017-01-30 17:27:35 +0100

    Set the movement of the seconds hand

--- Clock/src/controller/MainWindowController.java
+++ Clock/src/controller/MainWindowController.java
@@ -1,52 +1,96 @@
 package controller;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Calendar;
+
 import com.sun.javafx.geom.Point2D;
 
 import javafx.animation.Animation;
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
 import javafx.animation.RotateTransition;
+import javafx.animation.Timeline;
+import javafx.beans.property.DoubleProperty;
 import javafx.fxml.FXML;
 import javafx.geometry.Point3D;
+import javafx.scene.Group;
+import javafx.scene.layout.VBox;
 import javafx.scene.shape.Line;
+import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
-public class MainWindowController
-{
+public class MainWindowController {
 	private Stage primaryStage;
 	private Main main;
 	@FXML
-	private Line longHand;
+	private VBox minutesHand;
 	@FXML
-	private Line secondsHand;
+	private VBox secondsHand;
 	@FXML
-	private Line shortHand;
+	private VBox hoursHand;
 	@FXML
 	Animation animation;
 
-	public void setMain(Main main, Stage primaryStage)
-	{
+	public void setMain(Main main, Stage primaryStage) {
 		this.main = main;
 		this.primaryStage = primaryStage;
 	}
-	
-	public void setSecondsHandInMotion() 
-	{
+
+	public void setSecondsHandInMotion() {
 		RotateTransition rt = new RotateTransition();
 		rt.setNode(secondsHand);
-		rt.setDuration(Duration.seconds(60));
-		rt.setFromAngle(0);
-		rt.setToAngle(360);
+		rt.setInterpolator(Interpolator.LINEAR);
 		
+		rt.setByAngle(360);
+		rt.setDuration(Duration.seconds(60));
+		
+	
 		/*
-		for (int i=0; i<360; i+=10)
-		{
-			rt.setFromAngle(i);
-			rt.setToAngle(i+10);
-		}
+		Rotate secondsRotate = new Rotate(
+				6,
+				-100,
+				0);
+		secondsHand.getTransforms().add(secondsRotate);
 		*/
+		
 		rt.setCycleCount(Animation.INDEFINITE);
 		animation = rt;
 		rt.play();
+	}
+
+	void bindClockHandsToTime() {
 		
+		LocalTime now = LocalTime.now();
+		final double initialHoursHandDegrees = calculateHoursHandDegrees(now);
+		final double initialMinutesHandDegrees = calculateMinutesHandDegrees(now);
+		final double initialSecondsHandDegrees = calculateSecondsHandDegrees(now);
+
+	}
+	
+	/**
+	 * Performs a 360 degree rotation of the angleProperty once in every
+	 * duration. rotation starts from initialRotation degrees.
+	 */
+	
+	private void createRotationTimeline(DoubleProperty angleProperty, Duration duration, double initialRotation) {
+		Timeline timeline = new Timeline(
+				new KeyFrame(duration, new KeyValue(angleProperty, 360 + initialRotation, Interpolator.LINEAR)));
+		timeline.setCycleCount(Animation.INDEFINITE);
+		timeline.play();
+	}
+
+	private int calculateSecondsHandDegrees(LocalTime time) {
+		return time.getSecond() * (360 / 60);
+	}
+
+	private double calculateMinutesHandDegrees(LocalTime time) {
+		return (time.getMinute() / 360.0) * (360 / 60);
+	}
+
+	private double calculateHoursHandDegrees(LocalTime time) {
+		return (time.getHour() + calculateMinutesHandDegrees(time) / 360.0) * (360 / 12);
 	}
 }
