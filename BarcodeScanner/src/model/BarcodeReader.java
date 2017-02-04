f7c3732d9259255e83ea98ac448b0bf65d2be908
Karolina <karolajnaa@gmail.com>
2017-02-04 23:25:41 +0100

    Sorted out mess and returned to Hash Map with Strings

--- BarcodeScanner/src/model/BarcodeReader.java
+++ BarcodeScanner/src/model/BarcodeReader.java
@@ -3,22 +3,112 @@ package model;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Collections;
 
 public class BarcodeReader {
-	private ArrayList<String> barsList;
 
 	private String inputText;
 	private int lastLetterCount;
 	private char lastChar;
-	
+
+	private ArrayList<String> barsList;
+	private ArrayList<Integer> values;
+	private ArrayList<Integer> translatedValues;
+
 	private Map<Integer, ArrayList<String>> barsMap;
 	private Map<Integer, Integer> barsWidthMap;
+	private ArrayList<Integer> valuesList;
+
 
 	public void determineBarsInBarcode(String input) {
 		inputText = input;
 		barsList = prepareListOfBarsWithWidth(inputText);
 		barsMap = divideIntoSeparateArrays(barsList);
-		barsWidthMap = translateBarMapIntoIntegers(barsMap);
+		translatedValues = addValuesToList(barsMap);
+		
+	
+
+	}
+
+	public ArrayList<String> prepareListOfBarsWithWidth(String input) {
+		barsList = new ArrayList<String>();
+
+		inputText = input;
+		lastChar = inputText.charAt(0);
+		lastLetterCount = 1;
+
+		for (int i = 1; i < inputText.length(); i++) {
+			char currentChar = inputText.charAt(i);
+
+			if (currentChar == lastChar) {
+				lastLetterCount++;
+			} else {
+				addLastLetterWithNumberOfOccurrences();
+				lastLetterCount = 1;
+				lastChar = currentChar;
+			}
+		}
+		addLastLetterWithNumberOfOccurrences();
+		return barsList;
+	}
+
+	private void addLastLetterWithNumberOfOccurrences() {
+		if (lastLetterCount > 1) {
+
+			barsList.add(lastChar + Integer.toString(lastLetterCount));
+
+		} else {
+
+			barsList.add(lastChar + "1");
+		}
+	}
+
+	private ArrayList<Integer> addValuesToList(Map<Integer, ArrayList<String>> barsMap){
+
+		parseStringValuesIntoInts(barsMap);
+		replaceValuesWith1sAnd2sAccordingly();
+
+		for (int i : translatedValues)
+		{
+			System.out.println(i);
+		}
+
+		return translatedValues;	
+	}
+
+	private void parseStringValuesIntoInts(Map<Integer, ArrayList<String>> barsMap) {
+
+		for (int i = 0; i < barsMap.size(); i++) {
+			{
+				int value;
+				values = new ArrayList<>();
+
+				for (int j = 0; j <barsMap.get(i).size(); j++) {
+
+					String s = barsMap.get(i).get(j);
+					String sWidth = s.substring(1);
+					value = Integer.parseInt(sWidth);
+					values.add(value);		
+
+				}
+			}			
+		}
+	}
+
+	private void replaceValuesWith1sAnd2sAccordingly() {
+		int max = Collections.max(values);
+		translatedValues = new ArrayList<>();
+
+		for (int k = 0; k < values.size(); k++)
+		{
+
+			if (values.get(k) == max) {
+				translatedValues.add(2);
+			}
+			else {
+				translatedValues.add(1);
+			}
+		}
 	}
 
 	public Map<Integer, ArrayList<String>> divideIntoSeparateArrays(ArrayList<String> barsList) {
@@ -62,33 +152,43 @@ public class BarcodeReader {
 		barsMap.put(9, array10);
 		barsMap.put(10, array11);	
 
+		System.out.println(barsMap.values());
 		return barsMap;
 	}
 
 
-	private Map<Integer,Integer> translateBarMapIntoIntegers(Map<Integer, ArrayList<String>> barsMap) {
+	private Map<Integer, Integer> translateBarMapIntoIntegers(Map<Integer, ArrayList<String>> barsMap) {
 
 		barsWidthMap = new HashMap<>();
 
 		for (int i = 0; i < barsMap.size(); i++) {
-			
+
 			StringBuffer sb = new StringBuffer();
 
+			int value;
 			int barcodeAsNumber = 0;
-			
-			for (int j = 0; j < barsMap.get(i).size(); j++){
+
+			ArrayList<Integer> values = new ArrayList<>();
+
+			for (int j = 0; j < barsMap.get(i).size(); j++) {
 
 				String s = barsMap.get(i).get(j);
 				String sWidth = s.substring(1);
-				sb.append(sWidth);
+				value = Integer.parseInt(sWidth);
+				values.add(value);
 			}
-			String s = sb.toString();
-			barcodeAsNumber = Integer.parseInt(s);
-			barsWidthMap.put(i, barcodeAsNumber);
+
+
+
+			//	String s = sb.toString();
+			//	barcodeAsNumber = Integer.parseInt(s);
+			//	barsWidthMap.put(i, barcodeAsNumber);
 		}
+
 		System.out.println(barsWidthMap.values());
 		return barsWidthMap;
 	}
+
 
 	private void addListToMap(ArrayList<String> barsList, ArrayList<String> array, int range) {
 
@@ -103,33 +203,6 @@ public class BarcodeReader {
 		}
 	}
 
-	public ArrayList<String> prepareListOfBarsWithWidth(String input) {
-		barsList = new ArrayList<String>();
 
-		inputText = input;
-		lastChar = inputText.charAt(0);
-		lastLetterCount = 1;
 
-		for (int i = 1; i < inputText.length(); i++) {
-			char currentChar = inputText.charAt(i);
-
-			if (currentChar == lastChar) {
-				lastLetterCount++;
-			} else {
-				addLastLetterWithNumberOfOccurrences();
-				lastLetterCount = 1;
-				lastChar = currentChar;
-			}
-		}
-		addLastLetterWithNumberOfOccurrences();
-		return barsList;
-	}
-
-	private void addLastLetterWithNumberOfOccurrences() {
-		if (lastLetterCount > 1) {
-			barsList.add(lastChar + Integer.toString(lastLetterCount));
-		} else {
-			barsList.add(lastChar + "1");
-		}
-	}
 }
