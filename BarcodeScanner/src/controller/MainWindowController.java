a3db90630c7b38a9f28031a9d4d87ea6244365b3
Karolina <karolajnaa@gmail.com>
2017-02-04 12:07:28 +0100

    Part of layout done

---
+++ BarcodeScanner/src/controller/MainWindowController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import javafx.stage.Stage;
+
+public class MainWindowController {
+
+	private Stage primaryStage;
+	private Main main;
+	
+	public void setMain(Stage promaryStage, Main main)
+	{
+		this.primaryStage = primaryStage;
+		this.main = main;
+		
+	}
+}
