68d9c91e09395e220729e0731b82971928b78af9
KarolinaPru <karolajnaa@gmail.com>
2017-02-25 10:32:28 +0100

    Refactored C# class project

--- dotnet/Feb20workshop/Feb20workshop/Logic.cs
+++ dotnet/Feb20workshop/Feb20workshop/Logic.cs
@@ -1,18 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Feb20Class
+namespace Feb20workshop
 {
-    class Program
+    class Logic
     {
-        static void Main(string[] args)
-        {
-            Start();
-        }
-
         public static void Start()
         {
             Console.Write("Podaj x: ");
