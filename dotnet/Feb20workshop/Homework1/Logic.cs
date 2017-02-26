991daf4a41152d8ff920667b24aa018391fb318c
KarolinaPru <karolajnaa@gmail.com>
2017-02-26 18:12:49 +0100

    Testing the algorithm

--- dotnet/Feb20workshop/Homework1/Logic.cs
+++ dotnet/Feb20workshop/Homework1/Logic.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.Design.Serialization;
 using System.Linq;
+using System.Runtime.Serialization.Formatters;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,21 +10,59 @@ namespace Homework1
 {
     class Logic
     {
+        private int a;
+        private int b;
+        private int c;
+        private int d;
         public void Start()
         {
+            ObtainFourParametersFromUser();
+
+            string mask = new String('X', a+b+c+d);
+            Console.WriteLine("Wpisz tekst bez spacji o poprawnej długości: " + mask);
+
+            string inputText = Console.ReadLine();
+            string inputTransformed = inputText;
+
+            List<string> allCombinations = new List<string>();
+
+         //   do
+       //     {
+
+                for (int i = 0; i < 10; i++)
+                {
+                    string chunk1 = inputTransformed.Substring(0, a);
+                    string chunk2 = inputTransformed.Substring(a+1, b);
+                    string chunk3 = inputTransformed.Substring(b+1, c);
+                    string chunk4 = inputTransformed.Substring(c+1, d);
+
+                    inputTransformed = chunk2 + chunk4 + chunk1 + chunk3;
+
+                    Console.WriteLine(inputTransformed);
+                    //   allCombinations.Add(inputTransformed);
+                }
+                //    } while (inputText != inputTransformed);
+
+
+            Console.WriteLine(allCombinations.Distinct());
+
+            Console.WriteLine(inputText);
+            Console.ReadKey();
+        }
+
+        private void ObtainFourParametersFromUser()
+        {
             Console.Write("Podaj liczbę a: ");
-            int a = ObtainProperInputFromUser();
-           
+            a = ObtainProperInputFromUser();
+
             Console.Write("Podaj liczbę b: ");
-            int b = ObtainProperInputFromUser();
+            b = ObtainProperInputFromUser();
 
             Console.Write("Podaj liczbę c: ");
-            int c = ObtainProperInputFromUser();
+            c = ObtainProperInputFromUser();
 
             Console.Write("Podaj liczbę d: ");
-            int d = ObtainProperInputFromUser();
-            
-            Console.ReadKey();
+            d = ObtainProperInputFromUser();
         }
 
         private int ObtainProperInputFromUser()
@@ -38,7 +77,7 @@ namespace Homework1
             else
             {
                 Console.Write("Podaj poprawną liczbę: ");
-                ObtainProperInputFromUser();
+                return ObtainProperInputFromUser();
             }
             return i;
         }
